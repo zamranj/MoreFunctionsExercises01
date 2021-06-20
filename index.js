6aca8ba5f2e9c453c9f79339890b4ace8cd589fb
zeejay21 <84418586+zamranj@users.noreply.github.com>
2021-06-20 14:16:30 +0000

    Finished

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ function checkFuel(level) {
     return 'red';
   }
 }
-
+let nonSusFunction = function(a){
+  if(checkFuel(a) === "green"){
+    return a - 100001;
+  }
+  else if(checkFuel(a) === "yellow"){
+    return a - 500001;
+  }
+  else{
+    return a;
+  }
+};
 function holdStatus(arr){
   if (arr.length < 7) {
     return `Spaces available: ${7-arr.length}.`;
@@ -23,3 +33,19 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
+
+console.log("=====Test new function====\n");
+function deckMops(array){
+    let out=[];
+    return  out = array.splice(3, 2,"brick","xbox");
+    
+  }
+ 
+
+let innocentSwipe = function(levelOfFuel, itemsCargo){
+ let arr = deckMops(itemsCargo);
+ return `Raided ${nonSusFunction(levelOfFuel)}kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`
+}
+
+
+console.log(innocentSwipe(20000,cargoHold))
